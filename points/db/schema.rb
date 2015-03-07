63f945ad239d73d70ab7191902399900105f7ce0
euglazer <eugenestapler@gmail.com>
2015-03-08 12:23:44 +1300

    migrate to pg

--- points/db/schema.rb
+++ points/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150307111846) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "group_id"
@@ -53,8 +56,8 @@ ActiveRecord::Schema.define(version: 20150307111846) do
     t.datetime "updated_at"
   end
 
-  add_index "groups", ["email"], name: "index_groups_on_email", unique: true
-  add_index "groups", ["reset_password_token"], name: "index_groups_on_reset_password_token", unique: true
+  add_index "groups", ["email"], name: "index_groups_on_email", unique: true, using: :btree
+  add_index "groups", ["reset_password_token"], name: "index_groups_on_reset_password_token", unique: true, using: :btree
 
   create_table "points", force: :cascade do |t|
     t.integer  "group_id"
@@ -83,7 +86,7 @@ ActiveRecord::Schema.define(version: 20150307111846) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
